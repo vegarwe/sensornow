a0177ca67e835ba33ade3254cb18e5d3923c288e
Vegar Westerlund <vegarwe@gmail.com>
2022-02-27 20:48:56 +0100

    Add esp now sending

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
+#include <esp_now.h>
 #include <Wire.h>
+#include <WiFi.h>
 
-#include "config.h"
 
 #if defined(BME280_SENSOR)
 #include <Adafruit_Sensor.h>
@@ -20,13 +21,20 @@ static DHT dht(DHTPIN, DHT22);  // Initialize DHT sensor for normal 16mhz Arduin
 #endif
 
 
-#define SLEEP_TIME                          20 * 60 * 1000 * 1000L
-//#define SLEEP_TIME                           1 * 60 * 1000 * 1000L
-//#define SLEEP_TIME                           1 * 20 * 1000 * 1000L
+#if defined(SIMULATOR)
+    #define SLEEP_TIME       20 * 1000 * 1000L
+#else
+    #define SLEEP_TIME  20 * 60 * 1000 * 1000L
+#endif
 
-static HardwareSerial* debugger = NULL;
 
-void print_wakeup_reason() {
+static HardwareSerial*  debugger            = NULL;
+static uint8_t          broadcastAddress[]  = {0x24, 0x6F, 0x28, 0x60, 0x37, 0x29}; // AMS Display
+static uint8_t          broadcastChannel    = 1;
+
+
+void print_wakeup_reason()
+{
     if (! debugger) return;
 
     esp_sleep_wakeup_cause_t wakeup_reason;
@@ -45,35 +53,45 @@ void print_wakeup_reason() {
 }
 
 
+void goto_sleep(const char* reason)
+{
+    if (debugger)
+    {
+        debugger->print(reason);
+        debugger->print(" ");
+        debugger->println(millis());
+        debugger->flush();
+    }
+
+    esp_sleep_enable_timer_wakeup(SLEEP_TIME);
+    esp_deep_sleep_start();
+}
+
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+    goto_sleep(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+
 void vTaskFunction(void * pvParameters)
 {
     TickType_t xLastWakeTime = xTaskGetTickCount();
     TickType_t x20SecondsInTicks = (15000) / portTICK_RATE_MS;
 
-    if (cfgbuf.ip == 0) {
-        x20SecondsInTicks = (25500) / portTICK_RATE_MS;
-        if (debugger) debugger->println("Grant extra time");
-    }
-
-    // Wait for the next cycle.
     vTaskDelayUntil(&xLastWakeTime, x20SecondsInTicks);
 
-    success_count = 0;
-
-    Serial.print("giving up after ");
-    Serial.println(millis());
-    Serial.flush();
-
-    cfgbuf.ip = 0; // Invalidate stored WIFI settings
-    esp_sleep_enable_timer_wakeup(SLEEP_TIME);
-    esp_deep_sleep_start();
+    goto_sleep("giving up");
 
     vTaskDelete(NULL);
 }
 
 
-void setup() {
-    //debugger = &Serial;
+void setup()
+{
+#if defined(SIMULATOR)
+    debugger = &Serial;
+#endif
 
     if (debugger)
     {
@@ -100,12 +118,8 @@ void setup() {
     {
         debugger->print("battery measurement: ");
         debugger->println(battery_value);
-        // (2112 * 2 * 3.3 / 4095) + 0.366 ~= 3.77
-        // (2444 * 2 * 3.3 / 4095) + 0.366 ~= 4.2 -- 4.3
     }
 
-    ++boot_count;
-    if (debugger) debugger->println("Boot number: " + String(boot_count));
     print_wakeup_reason();
 
 #if   defined(BME280_SENSOR)
@@ -121,19 +135,34 @@ void setup() {
     // Start background timeout
     xTaskCreate(vTaskFunction, "vTaskFunction", 10000, (void *)1, tskIDLE_PRIORITY, NULL);
 
-    // TODO: Connect NOW
+    // Connect to gateway
+    WiFi.mode(WIFI_STA);
+    esp_now_init();
+    esp_now_register_send_cb(OnDataSent);
+
+    esp_now_peer_info_t peerInfo = {0};
+    memcpy(peerInfo.peer_addr, broadcastAddress, sizeof(broadcastAddress));
+    peerInfo.channel = broadcastChannel;
+    peerInfo.encrypt = false;
+
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+        goto_sleep("Failed to add peer");
+    }
+
 
     static char payload[512];
 #if   defined(BME280_SENSOR)
     // status = bme.begin(0x76, &Wire2)
     if (! bme.begin(BME280_ADDRESS_ALTERNATE)) {
-        Serial.println("ERR: Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(), 16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10); // TODO: Go back to sleep!
+        if (debugger)
+        {
+            debugger->print("SensorID was: 0x"); Serial.println(bme.sensorID(), 16);
+            debugger->print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+            debugger->print("   ID of 0x56-0x58 represents a BMP 280,\n");
+            debugger->print("        ID of 0x60 represents a BME 280.\n");
+            debugger->print("        ID of 0x61 represents a BME 680.\n");
+        }
+        goto_sleep("ERR: Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
     }
     float temperature   = bme.readTemperature();
     float pressure      = bme.readPressure() / 100.0F;
@@ -141,7 +170,7 @@ void setup() {
 
     digitalWrite(POWPIN, LOW);
 
-    sprintf(payload,"{ \"temperature\": %0.2f, \"pressure\": %0.2f, \"humidity\": %.2f, \"battery_voltage\": %u, \"millis\": %lu }",
+    sprintf(payload,"/sensor_now/data { \"temperature\": %0.2f, \"pressure\": %0.2f, \"humidity\": %.2f, \"battery_voltage\": %u, \"millis\": %lu }",
             temperature,
             pressure,
             humidity,
@@ -161,37 +190,33 @@ void setup() {
 
     digitalWrite(POWPIN, LOW);
 
-    sprintf(payload,"{ \"temperature\": %0.2f, \"humidity\": %.2f, \"battery_voltage\": %u, \"millis\": %lu }",
+    sprintf(payload,"/sensor_now/data { \"temperature\": %0.2f, \"humidity\": %.2f, \"battery_voltage\": %u, \"millis\": %lu }",
             temperature,
             humidity,
             battery_value,
             millis());
 #else
-    sprintf(payload,"{ \"battery_voltage\": %u, \"millis\": %lu }",
+    sprintf(payload,"/sensor_now/data { \"battery_voltage\": %u, \"millis\": %lu }",
             battery_value,
             millis());
 #endif
 
     if (debugger)
     {
+        debugger->printf("%d\n", strnlen(payload, sizeof(payload)));
         debugger->print(payload);
         debugger->println();
     }
 
-    // TODO: Now
-
-    esp_sleep_enable_timer_wakeup(SLEEP_TIME);
-    if (debugger)
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t*)payload, strnlen(payload, sizeof(payload)));
+    if (result != ESP_OK && debugger)
     {
-        debugger->print("sleeping after ");
-        debugger->println(millis());
-        debugger->flush();
+        debugger->println("Error sending the data");
     }
-    esp_deep_sleep_start();
 }
 
-//--------------------------------------------------------------------------------
 void loop()
 {
+    delay(10); // Wait for message sent callback
 }
 
